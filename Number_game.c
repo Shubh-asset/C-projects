1f7951e3ea8b872a8018cad8319b58a0aa32158b
Shubh-asset <workoho001@gmail.com>
2024-11-06 18:06:53 +0530

    game

--- Number_game.c
+++ Number_game.c
@@ -1,10 +1,40 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-int main()
+int main() 
 {
-    int a,b;
-    a=2;
-    b=34;
-    printf("the added number is %d", a+b);
+    int random, guess, no_of_guess=0;
+    srand(time(NULL));
+
+    printf("Welcome to the Guessing game (:\n");
+    random = rand() % 100 +1; //Generating number 1 to 100
+    
+    //printf("\nthe random number is %d\n", random);
+
+    do{
+
+        printf("Enter the number of your choice(1 to 100)\n");
+        scanf("%d", &guess);
+        no_of_guess++;
+
+        if(guess>random)
+        {
+            printf("guess a smaller number\n");
+        }
+        else if(guess<random)
+        {
+            printf("Guess the larger number please\n");
+        }
+        else
+        {
+            printf("Congratulation!! you guessed the right number in %d attempts\n", no_of_guess);
+        }
+
+    }while(random != guess);
+
+    printf("Hurray!! you finished the game");
+    printf("\ncredit: Prashant sir");
+
     return 0;
 }
