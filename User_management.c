92eb46e94695d0efeccb0b605877d8b486cb3298
Shubh-asset <workoho001@gmail.com>
2025-01-12 12:33:00 +0530

    cc

--- User_management.c
+++ User_management.c
@@ -1,130 +1,165 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//editing password input
+#include <unistd.h> // point to current terminal
+#include <stdlib.h>
+// terminal's function to be edited
+#include <termios.h> 
 
 
-#define MAX_USER  10
-#define CREDENTIAL_LENGTH  30
+#define MAX_USER 10
+#define CREDENTIALS_L 30
 
 typedef struct
 {
-    char username[CREDENTIAL_LENGTH];
-    char password[CREDENTIAL_LENGTH];
-}User;
+    char username[CREDENTIALS_L];
+    char password[CREDENTIALS_L];
+} User;
 
-User them[MAX_USER];
-int user_count=0;
+User user[MAX_USER];
+int user_count = 0;
 
-void register_user();
+void user_reg();
 int login_user();
-//helps in matching the register and login credentials of user
-void fix_fgets_input(char*);//backslash n ko backslash 0 krne ka input
-void input_credential(char* username, char* password);
+void fgets_input(char* string);
+void user_cred(char* username, char* password);
 
 int main()
 {
-    while(1)
-    {
-        int option;
-        int user_index;
-        printf("\n\n*Welcome toh user management*\n");
-        printf("\n1. Register");
-        printf("\n2. Login");
-        printf("\n3. Exit");
-        printf("\nEnter the option of your choice:\n");
-        scanf("%d", &option);
+    int option;
+    int user_index;
+while (1)
+{
+    printf("**User management**");
+    printf("\n1.Register");
+    printf("\n2.Login");
+    printf("\n3.Exit");
 
-        //To consume extra ENTER key which registers in input buffer of scanf function: thus consumes extra enter key
-        getchar();
+    
+    printf("\nEnter your choice: ");
+    scanf("%d", &option);
+    getchar(); //consume extra enter key
 
+    switch(option)
+    {   case 1:
+        printf("Kindly fill your credentials\n");
+        user_reg();
+        break;
 
-        switch (option)
+        case 2:
+        //printf("Welcome back %s", username);
+        user_index = login_user();
+        if(user_index>=0)
         {
-        case 1:
-            register_user();
-            break;
+            printf("login successfull !, welcome %s", user[user_index].username);
+        }
+        else
+        {
+            printf("Incorrect username or password!");
+        }
+        break;
 
-        case 2:  
-            user_index = login_user();
-            if (user_index >= 0)
-            {
-                printf("Login successfull !, Welcome %s", them[user_index].username);
-            }
-            else
-            {
-                printf("Incorrect username and password\n");
-            }
-            
         case 3:
-            printf("\nExiting the program.\n");
-            return 0;
+        printf("Exiting the software, Thank you\n");
+        break;
 
         default:
-            printf("Invalid option, please try again!");
-            break;
-        }
+        printf("Incorrect Option, please exiting the program\n");
+        break;
     }
+}
+
     return 0;
 }
 
-void register_user()
+void user_reg()
 {
-    if(user_count == MAX_USER)
+    if (user_count == MAX_USER)
     {
-        printf("\nMaximum user allowed are %d, Limit reached. Hence please try again later\n", MAX_USER);
+        printf("Maximum limit has been reached for User registration | the registered users are: %d\n", MAX_USER);
         return;
     }
+    else
+    {
+       int new_index = user_count;
+       printf("Register a new user\n");
 
-    int new_index = user_count;
-    printf("\nRegister a new user.");
-    
-        //scanf("%[^\n]s", alpha);  this bracket ensure that string will inserted till the new line is not entered 
-    
-    input_credential(them[new_index].username, them[new_index].password);
+       user_cred(user[new_index].username, user[new_index].password);
 
-    user_count++;
-    printf("\nRegistration successful\n");
-
-    //printf("%s-%s", them[new_index].username, them[new_index].password);
- 
+       user_count++;
+       printf("\nRegistration successful\n");
+    }
 }
 
 int login_user()
 {
-    char username[CREDENTIAL_LENGTH], password[CREDENTIAL_LENGTH];
-    // printf("Enter Username: ");
-    fgets(username, CREDENTIAL_LENGTH, stdin);
-    
-    fix_fgets_input(username);
-    input_credential(username, password);
+    char username[CREDENTIALS_L], password[CREDENTIALS_L];
+    fgets(username, CREDENTIALS_L, stdin);
 
-    
+    fgets_input(username);
+    user_cred(username, password);
+    printf("\n-%s-%s-\n", username, password);
 
-    for (int i = 0; i < user_count; i++)
+    for(int i = 0; i<user_count; i++)
     {
-        if(strcmp(username, them[i].username)==0 && strcmp(password, them[i].password)==0)
+        if(strcmp(username, user[i].username) == 0 && strcmp(password, user[i].password) == 0)
         {
             return i;
         }
     }
-    //printf("%s-%s", username, password);
-
     return -1;
 }
 
-void fix_fgets_input(char* string)
-{
-    int index = strcspn(string, "\n");
-    string[index] == "\0";
+
+void fgets_input(char* string)
+{//replacing the backslash n to backslash 0 toh make it null terminated string
+    int index = strcspn(string, "/n");
+    string[index] = '\0';
 }
 
-void input_credential(char* username, char* password)
+void user_cred(char* username, char* password)
 {
     printf("\nEnter Username: ");
-    fgets(username, CREDENTIAL_LENGTH, stdin);
-    
-    fix_fgets_input(username);
-    printf("\nEnter password: ");
-    fgets(password, CREDENTIAL_LENGTH, stdin);
-    fix_fgets_input(password);
+    fgets(username, CREDENTIALS_L, stdin);
+
+    fgets_input(username);
+    printf("enter password (masking enabled): \n");
+    fflush(stdout);
+
+    //Change terminal properties
+    struct termios old_props, new_props;
+    tcgetattr(STDIN_FILENO, &old_props);
+    new_props = old_props;
+    new_props.c_lflag = ~(ECHO | ICANON);
+
+    //TCSNOW: flags we changed should get visible easily
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_props);
+
+    char ch;
+    int i = 0;
+    //printf("enter password (masking enabled):");
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        if(ch == '\b' || ch == 127)
+        {
+            if(i>0)
+            {
+            i--;
+            printf("\b \b");
+            }
+                
+            }
+        
+        else
+        {
+        password[i++] = ch;
+        printf("*");
+        }    
+            
+    }
+    password[i] = '\0';
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_props);
+    //printf("Enter password: ");
+    //fgets(password, CREDENTIALS_L, stdin);
+    //fgets_input(password);
 }
